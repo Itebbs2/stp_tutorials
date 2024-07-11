480a0d898c220fd8227d813baa00c1590c60cd4d
Iwan <iwan.tebbs@postgrad.manchester.ac.uk>
2024-07-11 10:53:23 +0100

    initial commit

---
+++ Hello.py
@@ -0,0 +1,15 @@
+
+"""
+Script to say hello to a name of your choice
+"""
+
+def say_hi(name):
+    """
+    function that takes a name string as input and outputs a greeting to that name
+    :param name:
+    :return greeting:
+    """
+    print(f"hello, {name}")
+
+my_name = "Iwan"
+say_hi(my_name)
